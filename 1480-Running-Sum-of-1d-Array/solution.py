336535ed3f9928d9069deee74f238be1111ca270
Austin <aenloe@ucsd.edu>
2023-06-13 00:12:40 -0400

    1480 - start

---
+++ 1480-Running-Sum-of-1d-Array/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def runningSum(self, nums: list[int]) -> list[int]:
+        output = [] 
+        sum = 0
+        for number in nums:
+            sum += number
+            output.append(sum)
+
